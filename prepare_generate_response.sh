67107017f00a701dc5f2a1031b033b55a9cbb9d0
Thanapat Sorralump <thanapat423@gmail.com>
2022-03-23 15:21:06 +0700

    Update shell script

--- prepare_generate_response.sh
+++ prepare_generate_response.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]
     echo "Please insert graphql folder path to the first argument. Example: ./prepare_generate_response ../bdit-ios/Core/Core/GraphQL/"
 fi
 
-rm -r src/commonMain/graphql/query
-cp -r $1 src/commonMain/graphql/query
+rm -r src/commonMain/graphql
+cp -r $1 src/commonMain/graphql
